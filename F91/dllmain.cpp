644226d1a4bacd864de0443a9b10db603ce1f74d
hiroki-repo <hiroki7v11@gmail.com>
2022-09-09 13:58:56 +0900

    Add files via upload

--- F91/dllmain.cpp
+++ F91/dllmain.cpp
@@ -24,6 +24,7 @@ extern "C"{
 #endif
 int (*f91memaccess)(int, int, int);
 void (*f91spiaccess)(bool);
+void (*f91gpioack)(UINT32);
 void (*cpu_int)(int);
 void (*cpu_reset)(void);
 int (*cpu_execute)(void);
@@ -50,12 +51,53 @@ UINT8 flashicr = 0;
 UINT8 flashpcr = 0;
 UINT8 intpr[6];
 UINT8 chipselect[4][3] = { {0x00,0xff,0xe8},{0x00,0x00,0x00},{0x00,0x00,0x00},{0x00,0x00,0x00} };
+UINT8 GPIO[4][5] = { {0x00,0xff,0x00,0x00,0x00},{0x00,0xff,0x00,0x00,0x00},{0x00,0xff,0x00,0x00,0x00},{0x00,0xff,0x00,0x00,0x00} };
+UINT8 val4GPIOMode6 = 0;
 int pointer4accflash = 0;
 FILE* flashfdcrpt = 0;
 int ret = 0;
 
 __declspec(dllexport) void f91spimisocall(bool prm_0) { 
 	buf4rttmpx &= 0x7; if (buf4rttmpx == 0) { buf4rttmp = 0; spisr &= 0x7F; } buf4rttmp |= prm_0 << (buf4rttmpx++); if (buf4rttmpx == 7) { spisr |= 0x80; buf4rt[buf4acc++] = buf4rttmp; buf4rttmpx = 0; if (spicr & 0x80) { cpu_int(0x7c); } }
+}
+
+__declspec(dllexport) UINT8 f91gpiocall(UINT32 prm_0,UINT8 prm_1) {
+	UINT8 f91gpioretvalue = 0;
+	for (int cnt = 0; cnt < 8; cnt++) {
+		UINT8 f91gpioinmode = (((GPIO[prm_0][1] >> cnt) & 1) << 0) | (((GPIO[prm_0][2] >> cnt) & 1) << 1) | (((GPIO[prm_0][3] >> cnt) & 1) << 2);
+		switch (f91gpioinmode) {
+		case 0:
+			f91gpioretvalue &= (~(1 << cnt));
+			f91gpioretvalue |= ((((prm_1 & (~GPIO[prm_0][1])) >> cnt) & 1) << cnt);
+			break;
+		case 1:
+			GPIO[prm_0][0] &= (~(1 << cnt));
+			GPIO[prm_0][0] |= ((((prm_1 & GPIO[prm_0][1]) >> cnt) & 1) << cnt);
+			break;
+		case 2:
+			if ((GPIO[prm_0][0] >> cnt) & 1) { GPIO[prm_0][0] &= (~(1 << cnt)); GPIO[prm_0][0] |= ((((prm_1) >> cnt) & 1) << cnt); }else{ f91gpioretvalue &= (~(1 << cnt)); f91gpioretvalue &= (~(1 << cnt)); }
+			break;
+		case 3:
+			if ((GPIO[prm_0][0] >> cnt) & 1) { f91gpioretvalue &= (~(1 << cnt)); f91gpioretvalue |= ((1 << cnt)); }else{ GPIO[prm_0][0] &= (~(1 << cnt)); GPIO[prm_0][0] |= ((((prm_1) >> cnt) & 1) << cnt); }
+			break;
+		case 4:
+			if ((GPIO[prm_0][0] >> cnt) & 1) {
+				if (((((prm_1 & (~val4GPIOMode6)) & GPIO[prm_0][0]) & (~GPIO[prm_0][4])) >> cnt) & 1) { cpu_int(0x80 + (4 * cnt) + (32 * prm_0)); Sleep(1); }
+				val4GPIOMode6 &= (~(1 << cnt)); val4GPIOMode6 |= (((prm_1 >> cnt) & 1) << cnt);
+			}
+			break;
+		case 5:
+			f91gpioretvalue = 0;
+			break;
+		case 6:
+			if ((((~prm_1)^GPIO[prm_0][0]) >> cnt) & 1) { cpu_int(0x80+(4*cnt)+(32*prm_0)); Sleep(1); }
+			break;
+		case 7:
+			if (((prm_1^GPIO[prm_0][0]) >> cnt) & 1) { cpu_int(0x80+(4*cnt)+(32*prm_0)); Sleep(1); }
+			break;
+		}
+	}
+	return f91gpioretvalue;
 }
 
 #if 0
@@ -83,7 +125,7 @@ case 1:
 return false;
 #endif
 
-__declspec(dllexport) void f91macfuncset(int (*tmpfunc)(int, int, int), void (*tmpfunc2)(bool), void (*tmpfunc3)(int), void (*tmpfunc4)(void), int (*tmpfunc5)(void)) { f91memaccess = tmpfunc; f91spiaccess = tmpfunc2; cpu_int = tmpfunc3; cpu_reset = tmpfunc4; cpu_execute = tmpfunc5; }
+__declspec(dllexport) void f91macfuncset(int (*tmpfunc)(int, int, int), void (*tmpfunc2)(bool), void (*tmpfunc3)(int), void (*tmpfunc4)(void), int (*tmpfunc5)(void), void (*tmpfunc6)(UINT32)) { f91memaccess = tmpfunc; f91spiaccess = tmpfunc2; cpu_int = tmpfunc3; cpu_reset = tmpfunc4; cpu_execute = tmpfunc5; f91gpioack = tmpfunc6; }
 __declspec(dllexport) void f91internalflashpathset(char *tmpfname) { fname4if = tmpfname; }
 
 __declspec(dllexport) int f91_execute(void) { externaltime = 0; return cpu_execute() + externaltime; }
@@ -122,6 +164,10 @@ __declspec(dllexport) void f91_reset(void) {
 	chipselect[1][0] = 0x00,0x00,0x00;
 	chipselect[2][0] = 0x00,0x00,0x00;
 	chipselect[3][0] = 0x00,0x00,0x00;
+	GPIO[0][0] = 0x00, 0xff, 0x00, 0x00, 0x00;
+	GPIO[1][0] = 0x00, 0xff, 0x00, 0x00, 0x00;
+	GPIO[2][0] = 0x00, 0xff, 0x00, 0x00, 0x00;
+	GPIO[3][0] = 0x00, 0xff, 0x00, 0x00, 0x00;
 	cpu_reset();
 }
 
@@ -142,6 +188,7 @@ __declspec(dllexport) int mac4ez80dll(int prm_0, int prm_1, int prm_2) {
             }
         } else if ((flashaubr <= ((prm_0 >> 16) & 0xFF)) && (((flashaubr+3)&0xFF) >= ((prm_0 >> 16) & 0xFF)) && ((flashcr & 0x08) != 0)) {
 			externaltime += (flashcr >> 5);
+			if (fname4if == nullptr) { return 0; }
 			if ((&fname4if) == NULL) { return 0; }
 			flashfdcrpt = fopen(fname4if,"rb+");
 			if (&flashfdcrpt == 0) { flashfdcrpt = fopen(fname4if, "wb"); }
@@ -173,6 +220,29 @@ __declspec(dllexport) int mac4ez80dll(int prm_0, int prm_1, int prm_2) {
 			intpr[prm_0 - 0x10] = prm_1;
 			break;
 
+		case 0x96:
+		case 0x97:
+		case 0x98:
+		case 0x99:
+		case 0x9a:
+		case 0x9b:
+		case 0x9c:
+		case 0x9d:
+		case 0x9e:
+		case 0x9f:
+		case 0xa0:
+		case 0xa1:
+		case 0xa2:
+		case 0xa3:
+		case 0xa4:
+		case 0xa5:
+			if ((((prm_0 - 0x96) % 4) == 0) && (f91gpioack != nullptr)) { for (int cnt = 0; cnt < 8; cnt++) { UINT8 f91gpioinmode = (((GPIO[(prm_0 - 0x96) / 4][1] >> cnt) & 1) << 0) | (((GPIO[(prm_0 - 0x96) / 4][2] >> cnt) & 1) << 1) | (((GPIO[(prm_0 - 0x96) / 4][3] >> cnt) & 1) << 2); if ((f91gpioinmode == 0) || (f91gpioinmode == 2) || (f91gpioinmode == 3)) { f91gpioack((prm_0 - 0x96) / 4); break; } } }
+			GPIO[(prm_0 - 0x96) / 4][(prm_0 - 0x96) % 4] = prm_1;
+			break;
+		case 0xa6:
+		case 0xa7:
+			GPIO[prm_0 - 0xa6][4] = prm_1;
+			break;
 		case 0xa8:
 		case 0xa9:
 		case 0xaa:
@@ -211,16 +281,23 @@ __declspec(dllexport) int mac4ez80dll(int prm_0, int prm_1, int prm_2) {
             break;
 
         case 0xbc:
+			if (f91spiaccess == nullptr) { return 0; }
 			if (&f91spiaccess == NULL) { return 0; }
-            for (int i = 0; i < 8; i++) {
+			for (int i = 0; i < 8; i++) {
                 f91spiaccess(((prm_1 >> i)&1) ? true : false);
             }
             break;
+
+		case 0xce:
+		case 0xcf:
+			GPIO[prm_0 - 0xcc][4] = prm_1;
+			break;
 
 		case 0xf5:
 			break;
 		case 0xf6:
 			externaltime += flashfdr;
+			if (fname4if == nullptr) { return 0; }
 			if (&fname4if == NULL) { return 0; }
 			flashfdcrpt = fopen(fname4if, "rb+");
 			if (&flashfdcrpt == 0) { flashfdcrpt = fopen(fname4if, "wb"); }
@@ -278,6 +355,25 @@ __declspec(dllexport) int mac4ez80dll(int prm_0, int prm_1, int prm_2) {
 			return intpr[prm_0-0x10];
 			break;
 
+		case 0x96:
+		case 0x97:
+		case 0x98:
+		case 0x99:
+		case 0x9a:
+		case 0x9b:
+		case 0x9c:
+		case 0x9d:
+		case 0x9e:
+		case 0x9f:
+		case 0xa0:
+		case 0xa1:
+		case 0xa2:
+		case 0xa3:
+		case 0xa4:
+		case 0xa5:
+			return GPIO[(prm_0 - 0x96) / 4][(prm_0 - 0x96) % 4];
+			break;
+
 		case 0xa8:
 		case 0xa9:
 		case 0xaa:
@@ -332,6 +428,7 @@ __declspec(dllexport) int mac4ez80dll(int prm_0, int prm_1, int prm_2) {
 
 		case 0xf6:
 			externaltime += flashfdr;
+			if (fname4if == nullptr) { return 0; }
 			if (&fname4if == NULL) { return 0; }
 			flashfdcrpt = fopen(fname4if, "rb+");
 			if (&flashfdcrpt == 0) { flashfdcrpt = fopen(fname4if, "wb"); }
